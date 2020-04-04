67fa23d1ef8ad1f86b72f571340b71474ef3ffdb
Thanh Nguyen <thanh.n@kbtg.tech>
2020-04-04 09:52:21 +0700

    leetcode: max sub array

---
+++ leetcode/maximum_subarray_53/maximum-subarray.go
@@ -0,0 +1,24 @@
+package maximum_subarray_53
+
+import "math"
+
+// Time complexity: O(n)
+func maxSubArray(nums []int) int {
+	sum := math.MinInt32
+	maxSum := math.MinInt32
+
+	for _, i := range nums {
+		if sum+i > i {
+			sum += i
+
+		} else {
+			sum = i
+		}
+
+		if sum > maxSum {
+			maxSum = sum
+		}
+	}
+
+	return maxSum
+}
