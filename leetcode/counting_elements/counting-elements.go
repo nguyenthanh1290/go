65fc47222c70e13b9cf1e49d8c78f03ae8587811
Thanh Nguyen <thanh.n@kbtg.tech>
2020-04-07 14:57:38 +0700

    leetcode: counting elements

---
+++ leetcode/counting_elements/counting-elements.go
@@ -0,0 +1,20 @@
+package counting_elements
+
+// Time complexity: O(n)
+// Space complexity: O(n)
+func countElements(arr []int) int {
+	bucket := make(map[int]struct{}, len(arr))
+	count := 0
+
+	for _, a := range arr {
+		bucket[a] = struct{}{}
+	}
+
+	for _, a := range arr {
+		if _, ok := bucket[a+1]; ok {
+			count++
+		}
+	}
+
+	return count
+}
