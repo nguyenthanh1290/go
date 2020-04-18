ed8d429c5d0c579b04561b3006fd045153e37afa
Thanh Nguyen <thanh.n@kbtg.tech>
2020-04-18 13:51:58 +0700

    leetcode: number of islands

---
+++ leetcode/number_of_islands/number-of-islands.go
@@ -0,0 +1,35 @@
+package islands
+
+func numIslands(grid [][]byte) int {
+	if len(grid) == 0 {
+		return 0
+	}
+
+	num := 0
+
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			if grid[i][j] == '1' {
+				num++
+				dfs(grid, i, j) // sink the entire island
+			}
+		}
+	}
+
+	return num
+}
+
+func dfs(grid [][]byte, i, j int) {
+	if i < 0 || i >= len(grid) || j < 0 || j >= len(grid[i]) || grid[i][j] == '0' {
+		return
+	}
+
+	grid[i][j] = '0'
+
+	dfs(grid, i+1, j)
+	dfs(grid, i-1, j)
+	dfs(grid, i, j+1)
+	dfs(grid, i, j-1)
+
+	return
+}
