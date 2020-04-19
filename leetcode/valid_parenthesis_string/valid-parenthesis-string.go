2ab908de3103e5c0bac3018726cacd7c2f48316c
Thanh Nguyen <thanh.n@kbtg.tech>
2020-04-19 16:08:32 +0700

    leetcode: valid parenthesis string

--- leetcode/valid_parenthesis_string/valid-parenthesis-string.go
+++ leetcode/valid_parenthesis_string/valid-parenthesis-string.go
@@ -1,9 +1,55 @@
 package string
 
-// Approach #3: Greedy
+// Approach: Stack
+// Time Complexity: O(N)
+// Space Complexity: O(N)
+func checkValidString(s string) bool {
+	if len(s) == 0 {
+		return true
+	}
+
+	openStack := []int{}
+	starStack := []int{}
+
+	for i, r := range s {
+		switch r {
+		case '(':
+			openStack = append(openStack, i)
+		case '*':
+			starStack = append(starStack, i)
+		case ')':
+			if len(openStack) != 0 {
+				openStack = openStack[:len(openStack)-1]
+
+			} else if len(starStack) != 0 {
+				starStack = starStack[:len(starStack)-1]
+
+			} else {
+				return false
+			}
+		}
+	}
+
+	for lgh := len(openStack); lgh != 0; lgh = len(openStack) {
+		if len(starStack) == 0 {
+			return false
+
+		} else if openStack[lgh-1] < starStack[len(starStack)-1] {
+			openStack = openStack[:lgh-1]
+			starStack = starStack[:len(starStack)-1]
+
+		} else {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Approach: Greedy
 // Time Complexity: O(N)
 // Space Complexity: O(1)
-func checkValidString(s string) bool {
+func checkValidStringGreedy(s string) bool {
 	lo := 0
 	hi := 0
 
--- leetcode/valid_parenthesis_string/valid-parenthesis-string_test.go
+++ leetcode/valid_parenthesis_string/valid-parenthesis-string_test.go
@@ -32,6 +32,27 @@ func Test_checkValidString(t *testing.T) {
 			},
 			want: true,
 		},
+		{
+			name: "",
+			args: args{
+				s: "(*()",
+			},
+			want: true,
+		},
+		{
+			name: "",
+			args: args{
+				s: "(())((())()()(*)(*()(())())())()()((()())((()))(*",
+			},
+			want: false,
+		},
+		{
+			name: "",
+			args: args{
+				s: "((()))()(())(*()()())**(())()()()()((*()*))((*()*)",
+			},
+			want: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
