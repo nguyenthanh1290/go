666fb36cd73535d6b95b1f5178ef855e4d2281aa
Thanh Nguyen <thanh.n@kbtg.tech>
2020-05-20 16:32:43 +0700

    moved to other repos

--- cracking_the_coding_interview/core/concepts/bit_manipulation/check-if-ith-bit-is-set.go
+++
@@ -1,8 +0,0 @@
-package bitmnp
-
-// Check if the ith bit is set in the binary form of the given number.
-// Time complexity: O(1)
-// Space complexity: O(1)
-func isSet(n int, i uint) bool {
-	return (n & (1 << i)) != 0
-}
