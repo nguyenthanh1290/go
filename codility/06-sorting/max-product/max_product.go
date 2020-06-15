c684429bb32b2141812ff41ebdca0422604ab950
Thanh Nguyen <thanh.n@kbtg.tech>
2020-06-15 15:14:31 +0700

    update

--- codility/06-sorting/max-product/max_product.go
+++ codility/06-sorting/max-product/max_product.go
@@ -16,3 +16,34 @@ func Solution(A []int) int {
 	}
 	return lMax
 }
+
+// Time complexity: O(n)
+// Space complexity: O(1)
+func useSingleScan(nums []int) int {
+	min1, min2 := 1<<63-1, 1<<63-1
+	max1, max2, max3 := -1<<63, -1<<63, -1<<63
+
+	for _, n := range nums {
+		if n <= min1 {
+			min1, min2 = n, min1
+		} else if n <= min2 {
+			min2 = n
+		}
+
+		if n >= max1 {
+			max1, max2, max3 = n, max1, max2
+		} else if n >= max2 {
+			max2, max3 = n, max2
+		} else if n >= max3 {
+			max3 = n
+		}
+	}
+
+	p1 := min1 * min2 * max1
+	p2 := max3 * max2 * max1
+
+	if p1 > p2 {
+		return p1
+	}
+	return p2
+}
